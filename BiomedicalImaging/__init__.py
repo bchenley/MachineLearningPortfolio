922bd9e163cc4b7e2bb9d84c78f5e6fc03ef0ba3
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-11-17 23:03:12 -0500

    Create __init__.py

---
+++ BiomedicalImaging/__init__.py
@@ -0,0 +1,16 @@
+print("Initializing TorchTimeSeries package...")
+
+import importlib
+
+__all__ = ['acces_images',
+           'get_images',
+           'register_image',
+           'register_subject_images']
+
+for module_name in __all__:
+    module = importlib.import_module(f'.{module_name}', __name__)
+    globals()[module_name] = getattr(module, module_name)
+
+
+           
+print("Done")
