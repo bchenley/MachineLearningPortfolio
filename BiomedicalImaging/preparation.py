57fca1e7cf97d338f2fb10b5889d1ad2bba4e772
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-11-19 22:16:24 -0500

    Update preparation.py

--- BiomedicalImaging/preparation.py
+++ BiomedicalImaging/preparation.py
@@ -16,35 +16,35 @@ def validate_input(input_text, prompt):
         if confirm == 'y':
             return user_input
 
-# Initialize variables
-task_path = ""
-save_dir = ""
-sample_size = 100
-train_size = 0
-test_size = 0
-register_images = False
-
-# '/content/drive/MyDrive/data/MSD/Task01_BrainTumour'
-
-# Get user input and validate
-task_path = validate_input("path to DICOM dataset", "Enter path to DICOM dataset:")
-save_dir = validate_input("path to save images", "Enter path to save images:")
-sample_size = int(validate_input("sample size (default = 100)", "Enter sample size (default = 100):"))
-train_size = int(sample_size * float(validate_input("% training size [0, 1]", "Enter % training size [0, 1]:")))
-test_size = sample_size - train_size
-register_images_input = validate_input("register Images to T1 (y/n)", "Register Images to T1 (y/n)?:")
-register_images = True if register_images_input == 'y' else False
-
-# Display user input for confirmation
-print(f"------------------------------------")
-print(f"Path to DICOM dataset = {task_path}")
-print(f"Path to save images = {save_dir}")
-print(f"Desired Sample size = {sample_size}")
-print(f"Training size = {train_size}")
-print(f"Test size = {test_size}")
-print(f"T1 Image Registration {'enabled.' if register_images else 'disabled.'}")
-
 if __name__ == '__main__':
+
+    # Initialize variables
+    task_path = ""
+    save_dir = ""
+    sample_size = 100
+    train_size = 0
+    test_size = 0
+    register_images = False
+    
+    # '/content/drive/MyDrive/data/MSD/Task01_BrainTumour'
+    
+    # Get user input and validate
+    task_path = validate_input("path to DICOM dataset", "Enter path to DICOM dataset:")
+    save_dir = validate_input("path to save images", "Enter path to save images:")
+    sample_size = int(validate_input("sample size (default = 100)", "Enter sample size (default = 100):"))
+    train_size = int(sample_size * float(validate_input("% training size [0, 1]", "Enter % training size [0, 1]:")))
+    test_size = sample_size - train_size
+    register_images_input = validate_input("register Images to T1 (y/n)", "Register Images to T1 (y/n)?:")
+    register_images = True if register_images_input == 'y' else False
+    
+    # Display user input for confirmation
+    print(f"------------------------------------")
+    print(f"Path to DICOM dataset = {task_path}")
+    print(f"Path to save images = {save_dir}")
+    print(f"Desired Sample size = {sample_size}")
+    print(f"Training size = {train_size}")
+    print(f"Test size = {test_size}")
+    print(f"T1 Image Registration {'enabled.' if register_images else 'disabled.'}")
     
     ## Get images, performing registration if desired
     images = access_images(task_path = task_path,
