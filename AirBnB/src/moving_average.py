3d512be2a284e68a7a420d21339ce91fa45e06be
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-11-25 09:19:31 -0500

    Update moving_average.py

--- AirBnB/src/moving_average.py
+++ AirBnB/src/moving_average.py
@@ -12,24 +12,24 @@ def moving_average(X, window):
         y: The output signal after applying the moving average filter.
     '''
 
+    N = X.shape[0]
+
     if window is None:
-      window = np.ones((np.arange(X.shape[0]), 1))
+      window = np.ones((np.arange(N), 1))
 
     len_window = window.shape[0]
 
     y = np.empty_like(X)
 
-    ww = []
-
     for i in range(X.shape[0]):
         is_odd = int(np.mod(len_window, 2) == 1)
 
-        m = np.arange((i - (len_window - is_odd) / 2), (i + (len_window - is_odd) / 2 - (is_odd == 0) + 1), dtype = np.int8)
-
-        k = m[(m >= 0) & (m < X.shape[0])]
-
-        window_ = window[(m >= 0) & (m < X.shape[0])]
-
+        m = np.arange((i - (len_window - is_odd) / 2), (i + (len_window - is_odd) / 2 - (is_odd == 0) + 1), dtype = np.int32)
+        
+        k = m[(m >= 0) & (m < N)]
+    
+        window_ = window[(m >= 0) & (m < N)]
+        
         window_ /= window_.sum(0)
 
         y[i] = np.matmul(window_, X[k])
