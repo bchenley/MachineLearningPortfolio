eeb139af3b3b182ecab658072577f7c50cc5d7d5
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-11-30 16:08:22 -0500

    Update unzip.py

--- SentimentAnalysis/unzip.py
+++ SentimentAnalysis/unzip.py
@@ -1,6 +1,6 @@
 def unzip(source, sink = None):
   
-  sink = sink or os.path.dirnmane(source)
+  sink = sink or os.path.dirname(source)
 
   with zipfile.ZipFile(source, 'r') as zip_ref:
     zip_ref.extractall(sink)
