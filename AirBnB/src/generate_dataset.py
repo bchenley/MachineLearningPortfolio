774e457808b4ec918373831567f3cd5a52ed8983
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-12-02 22:16:03 -0500

    Update generate_dataset.py

--- AirBnB/src/generate_dataset.py
+++ AirBnB/src/generate_dataset.py
@@ -1,173 +1,102 @@
 import pandas as pd
 
-def generate_dataset(df_master: pd.DataFrame,
-                     listing_neighborhoods: list = None,
-                     guest_country: list = None,
-                     host_country: list = None,
-                     room_type: list = None,
-                     interval: str = 'hour',
-                     look_back: int = 1):
-
-  df = df_master.copy()
-  
-  if listing_neighborhoods is not None:
-    mask = df['listing_neighborhood'].isin(listing_neighborhoods)
-    df = df[mask].reset_index(drop = True)
-
-  if guest_country is not None:
-    mask = df['guest_country'].isin(guest_country)
-    df = df[mask].reset_index(drop = True)
-  
-  if host_country is not None:
-    mask = df['host_country'].isin(host_country)
-    df = df[mask].reset_index(drop = True)
-  
-  if room_type is not None:
-    mask = df['room_type'].isin(room_type)
-    df = df[mask].reset_index(drop = True)
+def generate_dataset(df: pd.DataFrame,
+                     interval: str = 'D',
+                     room_types: list = None,
+                     guest_countries: list = None,
+                     listing_neighborhoods: list = None, 
+                     fix_outliers = False):
     
-  # Convert timestamps to datetimes
-  df.ts_interaction_first = pd.to_datetime(df.ts_interaction_first) # .dt.floor('D' if interval == 'day' else 'H')
-  df.ts_reply_at_first = pd.to_datetime(df.ts_reply_at_first) # .dt.floor('D' if interval == 'day' else 'H')
-  df.ts_accepted_at_first = pd.to_datetime(df.ts_accepted_at_first) # .dt.floor('D' if interval == 'day' else 'H')
-  df.ts_booking_at = pd.to_datetime(df.ts_booking_at) # .dt.floor('D' if interval == 'day' else 'H')
-
-  # Get min and max times
-  max_time = min(max(df.ts_interaction_first), max(df.ts_reply_at_first), max(df.ts_accepted_at_first), max(df.ts_booking_at))
-  min_time = max(min(df.ts_interaction_first), min(df.ts_reply_at_first), min(df.ts_accepted_at_first), min(df.ts_booking_at))
-
-  time = pd.date_range(start = min_time, end = max_time, freq = '1D' if interval == 'day' else '1H')
-  day = time.strftime('%A')
-  
-  if interval == 'day':
-    time = time.strftime('%Y-%m-%d')
-  else:
-    time = time.strftime('%Y-%m-%d %H:00')
-
-  N = len(time)
-
-  zeros_ = np.zeros((N-1,))
-  nans_ = np.ones((N-1,)) * np.nan
-  
-  df_interval = pd.DataFrame({'date': time[1:],
-                              'day': day[1:],
-                              'n_guests': zeros_,
-                              'n_hosts': zeros_,
-                              'n_listings': zeros_,
-                              'n_inquiries': zeros_,
-                              'n_replies': zeros_,
-                              'n_accepted': zeros_,
-                              'n_instant_book': zeros_,
-                              'n_non_instant_book': zeros_,
-                              'book_rate': zeros_,
-                              'instant_book_rate': zeros_,
-                              'accept_rate': zeros_,                              
-                              'non_instant_book_rate': zeros_,
-                              'accept_lag': nans_,
-                              'non_instant_book_lag': zeros_})
-  
-  day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  df_interval['day'] = pd.Categorical(df_interval['day'], categories = day_order, ordered = True)
-
-  interaction_isnot_null = ~df.ts_interaction_first.isnull()
-  is_instant_book = df.contact_channel_first == 'instant_book'
-  isnot_instant_book = df.contact_channel_first != 'instant_book'
-
-  for i in range(1, len(time)):
-
-    # Start and end times of current time point (day or hour)
-    min_time_i, max_time_i = time[i-1], time[i]
-
-    interaction_in_interval = (df.ts_interaction_first > min_time_i) & (df.ts_interaction_first <= max_time_i) & ~df.ts_interaction_first.isnull()
-    reply_in_interval = (df.ts_reply_at_first > min_time_i) & (df.ts_reply_at_first <= max_time_i) & ~df.ts_reply_at_first.isnull()
-    accept_in_interval = (df.ts_accepted_at_first > min_time_i) & (df.ts_accepted_at_first <= max_time_i) & ~df.ts_accepted_at_first.isnull()
-    book_in_interval = (df.ts_booking_at > min_time_i) & (df.ts_booking_at <= max_time_i) & ~df.ts_booking_at.isnull()
+    df_ts = df.copy()
     
-    inquiry_idx_i = np.where(interaction_isnot_null & interaction_in_interval)[0]
-    non_instant_book_inquiry_idx_i = np.where(isnot_instant_book & interaction_in_interval)[0]
-
-    replies_idx_i = np.where(isnot_instant_book & reply_in_interval)[0]
-    accepted_idx_i = np.where(isnot_instant_book & accept_in_interval)[0]
-    book_idx_i = np.where(book_in_interval)[0]
-    instant_book_idx_i = np.where(is_instant_book & book_in_interval)[0]
-    non_instant_book_idx_i = np.where(isnot_instant_book & book_in_interval)[0]
-
-    contact_is_contact_me = df.contact_channel_first[inquiry_idx_i] == 'contact_me'
-    contact_is_book_it = df.contact_channel_first[inquiry_idx_i] == 'book_it'
-    contact_is_instant_book = df.contact_channel_first[inquiry_idx_i] == 'instant_book'
-
-    n_contact_me_i = sum(contact_is_contact_me)
-    n_book_it_i = sum(contact_is_book_it)
-    n_instant_book_i = sum(contact_is_instant_book)
-
-    n_inquiries_i = len(inquiry_idx_i)
-    n_non_instant_inquiries_i = len(non_instant_book_inquiry_idx_i)
-
-    unique_guests_i = set(df.id_guest_anon[inquiry_idx_i])
-    unique_hosts_i = set(df.id_host_anon[inquiry_idx_i])
-    unique_listings_i = set(df.id_listing_anon[inquiry_idx_i])
-
-    n_guests_i = len(unique_guests_i)
-    n_hosts_i = len(unique_hosts_i)
-    n_listings_i = len(unique_listings_i)
+    if room_types is not None:
+        df_ts = df_ts[df_ts['room_type'].isin(room_types)]
     
-    n_replies_i = len(replies_idx_i)
-    n_accepted_i = len(accepted_idx_i)
-
-    n_book_i = len(book_idx_i)
-    n_instant_book_i = len(instant_book_idx_i)
-    n_non_instant_book_i = len(non_instant_book_idx_i)
-
-    t_accept_i = df.ts_accepted_at_first[non_instant_book_inquiry_idx_i]
-    t_interaction_i = df.ts_interaction_first[non_instant_book_inquiry_idx_i]
-    t_non_instant_book_i = df.ts_booking_at[non_instant_book_idx_i]
-
-    rate = lambda x,y: x/y if y != 0 else 0
+    if guest_countries is not None:
+        df_ts = df_ts[df_ts['guest_country'].isin(guest_countries)]
     
-    if n_inquiries_i > 0: 
+    if listing_neighborhoods is not None:
+        df_ts = df_ts[df_ts['listing_neighborhood'].isin(listing_neighborhoods)]
+    
+    df_ts_non_instant = df_ts[df_ts['contact_channel_first'] != 'instant_book'].copy()
 
-        # Total unique guests
-        df_interval.loc[df_interval.index[i-1], 'n_guests'] = n_guests_i
+    # Whether a booking occured or not
+    df_ts['booked'] = (~df_ts['ts_booking_at'].isna()).astype(int)
 
-        # Total unique hosts
-        df_interval.loc[df_interval.index[i-1], 'n_hosts'] = n_hosts_i
+    # Whether a guest is a previous booker or not
+    df_ts['past_booker'] = (df_ts['guest_user_stage_first'] == 'past_booker').astype(int)
+    df_ts['past_booker_booked'] = df_ts['past_booker'] * df_ts['booked']
 
-        # Total unique listings
-        df_interval.loc[df_interval.index[i-1], 'n_listings'] = n_listings_i
+    # Whether a non-instant booking occured or not
+    df_ts_non_instant['booked'] = (~df_ts_non_instant['ts_booking_at'].isna()).astype(int)    
 
-        # Total unique inquiries
-        df_interval.loc[df_interval.index[i-1], 'n_inquiries'] = n_inquiries_i
+    # Whether a non-instant guest is a previous booker or not
+    df_ts_non_instant['past_booker'] = (df_ts_non_instant['guest_user_stage_first'] == 'past_booker').astype(int)
+    df_ts_non_instant['past_booker_booked'] = df_ts_non_instant['past_booker'] * df_ts_non_instant['booked']
 
-        # Total unique replies
-        df_interval.loc[df_interval.index[i-1], 'n_replies'] = n_replies_i
+    # Time for the host to respond to non-instant inquiry
+    df_ts_non_instant['response_time'] = df_ts_non_instant['ts_reply_at_first'] - df_ts_non_instant['ts_interaction_first']
+    
+    df_ts.set_index('ts_interaction_first', inplace = True)
+    df_ts_non_instant.set_index('ts_interaction_first', inplace = True)
+    
+    interval_data = df_ts.resample(interval).agg({
+        'id_guest_anon': 'count', # total inquiries
+        'booked': 'sum', # total booked
+        'past_booker': 'sum', # total previous bookers
+        'past_booker_booked': 'sum' # total bookings by previous bookers
+        })
+    
+    interval_data_non_instant = df_ts_non_instant.resample(interval).agg({
+        'id_guest_anon': 'count', # total non-instant inquiries
+        'ts_reply_at_first': 'count',
+        'booked': 'sum', # total non-instant booked
+        'past_booker': 'sum', # total non-instant previous nbookers
+        'past_booker_booked': 'sum', # total non-instant bookings by previous bookers
+        'm_first_message_length_in_characters': 'sum', # total guest message length
+        'm_interactions': 'sum', # total guest-host interaction
+        'response_time': 'mean' # average response time
+        })
+    
+    interval_data_final = pd.DataFrame()
+    interval_data_final.index = interval_data.index
 
-        # Total unique accepted
-        df_interval.loc[df_interval.index[i-1], 'n_accepted'] = n_accepted_i
+    ## Metrics
+    # Conversion Rate 
+    interval_data_final['cr'] = interval_data['booked'] / interval_data['id_guest_anon']
 
-        # Total unique instant book
-        df_interval.loc[df_interval.index[i-1], 'n_instant_book'] = n_instant_book_i
+    # Average Response Time
+    interval_data_final['rt'] = interval_data_non_instant['response_time'].dt.total_seconds()
+    interval_data_final['rt'] /= 3600 if interval == 'D' else 60
 
-        # Total unique non-instant book
-        df_interval.loc[df_interval.index[i-1], 'n_non_instant_book'] = n_non_instant_book_i
+    # Average Response Rate
+    interval_data_final['rr'] = interval_data_non_instant['ts_reply_at_first'] / interval_data_non_instant['id_guest_anon']
 
-        # book_rate
-        df_interval.loc[df_interval.index[i-1], 'book_rate'] = rate(n_book_i, n_inquiries_i)
+    # Repeat Gueast Rate
+    interval_data_final['rgr'] = interval_data_non_instant['past_booker'] / interval_data_non_instant['id_guest_anon']
 
-        # instant_book_rate
-        df_interval.loc[df_interval.index[i-1], 'instant_book_rate'] = rate(n_instant_book_i, n_book_i)
+    # Repeat booking Rate
+    interval_data_final['rbr'] = interval_data_non_instant['past_booker_booked'] / interval_data_non_instant['booked']
 
-        # non_instant_book_rate
-        df_interval.loc[df_interval.index[i-1], 'non_instant_book_rate'] = rate(n_non_instant_book_i, n_accepted_i)
+    # Average length of guests' first message
+    interval_data_final['msg'] = interval_data_non_instant['m_first_message_length_in_characters'] / interval_data_non_instant['id_guest_anon']
 
-        # accept_rate
-        df_interval.loc[df_interval.index[i-1], 'accept_rate'] = rate(n_accepted_i, n_non_instant_inquiries_i)
+    # Average length of guest-host interaction
+    interval_data_final['ghi'] = interval_data_non_instant['m_interactions'] / interval_data_non_instant['id_guest_anon']
 
-        # accept_lag
-        df_interval.loc[df_interval.index[i-1], 'accept_lag'] = (t_accept_i - t_interaction_i).dropna().mean().seconds
-        df_interval.loc[df_interval.index[i-1], 'accept_lag'] /= 3600 if interval == 'hour' else 3600*24
+    interval_data_final['day'] = interval_data_final.index.strftime('%A')
 
-        # non_instant_book_lag
-        df_interval.loc[df_interval.index[i-1], 'non_instant_book_lag'] = (t_non_instant_book_i - t_accept_i).dropna().mean().seconds
-        df_interval.loc[df_interval.index[i-1], 'non_instant_book_lag'] /= 3600 if interval == 'hour' else 3600*24
+    day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    interval_data_final['day'] = pd.Categorical(interval_data_final['day'], categories = day_order, ordered = True)
 
-  return df_interval
+    if fix_outliers:
+        numeric_columns = interval_data_final.select_dtypes(include=['number']).columns
+        for col in numeric_columns:
+            median_i = interval_data_final[col].median()
+            iqr = interval_data_final[col].quantile(0.75) - interval_data_final[col].quantile(0.25)
+            lower_threshold = interval_data_final[col].quantile(0.25) - 1.5 * iqr
+            upper_threshold = interval_data_final[col].quantile(0.75) + 1.5 * iqr
+            outlier_condition = ~interval_data_final[col].between(lower_threshold, upper_threshold)
+            interval_data_final.loc[outlier_condition, col] = median_i
+
+    return interval_data_final
