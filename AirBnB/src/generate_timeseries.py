a6da30e93d519e8cf9f6d4fd80968c72ac4cdede
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-12-09 18:46:59 -0500

    Update generate_timeseries.py

--- AirBnB/src/generate_timeseries.py
+++ AirBnB/src/generate_timeseries.py
@@ -86,4 +86,6 @@ def generate_timeseries(df, interval='D', segments = None, values = None):
     month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October' , 'November', 'December']
     master_df['month'] = pd.Categorical(master_df['month'], categories = month_order, ordered = True)
     
+    master_df['year'] = master_df.index.year_name()
+
     return master_df
