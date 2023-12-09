7bceae657ac1c32079c2566f4017ebfe2d9f07e9
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2023-12-09 17:51:11 -0500

    Update generate_dataset.py

--- AirBnB/src/generate_dataset.py
+++ AirBnB/src/generate_dataset.py
@@ -1,102 +1,80 @@
 import pandas as pd
 
-def generate_dataset(df: pd.DataFrame,
-                     interval: str = 'D',
-                     room_types: list = None,
-                     guest_countries: list = None,
-                     listing_neighborhoods: list = None, 
-                     fix_outliers = False):
+def generate_dataset(df, interval='D', segments = None, values = None):
+
+    df = df.copy()
+
+    df['ts_interaction_first'] = pd.to_datetime(df['ts_interaction_first'])    
+    df.set_index('ts_interaction_first', inplace=True)
+    df.index.name = 'inquiry_date'
     
-    df_ts = df.copy()
+    time = df.resample(interval).count().index
     
-    if room_types is not None:
-        df_ts = df_ts[df_ts['room_type'].isin(room_types)]
+    master_df = pd.DataFrame(index = time)
+    master_df.index.name = df.index.name
+
+    # Define a list of all the functions to be applied
+    metrics_functions = [
+        calculate_inquiry_rate,
+        calculate_listing_rate,
+        calculate_avg_user_profile_completeness,  # Special handling for this function
+        calculate_host_response_rate, 
+        calculate_host_approval_rate, 
+        calculate_booking_conversion_rate, 
+        calculate_avg_response_time, 
+        calculate_avg_approval_time, 
+        calculate_avg_booking_time, 
+        calculate_avg_stay_time, 
+        calculate_avg_engagement, 
+        calculate_avg_reviews
+    ]
+
+    # Metric names without 'calculate_' prefix
+    metric_names = [func.__name__.replace('calculate_', '') for func in metrics_functions]
+
+    if segments is not None:
+        for segment in segments:
+            # unique_values = df[segment].unique()
+            
+            for value in values:
+                # Initialize a DataFrame for this segment value
+                # segment_df = pd.DataFrame(index=master_df.index)                
+                for func, metric_name in zip(metrics_functions, metric_names):
+                    if func == calculate_avg_user_profile_completeness:
+                        for user_type in ['guest', 'host']:
+                            # Call the function with additional user_type parameter
+                            result = func(df, variable = segment, value = value, user=user_type, interval=interval)
+
+                            master_df = master_df.join(pd.DataFrame(result, index = master_df.index), how='left')
+                            
+                    else:
+                        # Call the function with the segment and value
+                        result = func(df, variable = segment, value = value, interval=interval)
+                        
+                        master_df = master_df.join(pd.DataFrame(result, index = master_df.index), how='left')
+                
+                # # Add the segment value to the DataFrame
+                # segment_df[segment] = value
+                # # Concatenate this segment DataFrame to the master DataFrame
+                # master_df = pd.concat([master_df, segment_df], axis=0)
+            
+    else:
+        for func, metric_name in zip(metrics_functions, metric_names):
+            if func == calculate_avg_user_profile_completeness:
+                for user_type in ['guest', 'host']:
+                    # Call the function with additional user_type parameter
+                    result = func(df, user=user_type, interval=interval)
+                    
+                    master_df = master_df.join(pd.DataFrame(result, index = master_df.index), how='left')
+            else:
+                # Call the function for the entire dataset
+                result = func(df, interval=interval)
+
+                master_df = master_df.join(pd.DataFrame(result, index = master_df.index), how='left')
+                
+                
+    # # Rename the index to 'inquiry_date'
+    # master_df.index.name = 'inquiry_date'
+    # master_df.reset_index(inplace=True)
     
-    if guest_countries is not None:
-        df_ts = df_ts[df_ts['guest_country'].isin(guest_countries)]
-    
-    if listing_neighborhoods is not None:
-        df_ts = df_ts[df_ts['listing_neighborhood'].isin(listing_neighborhoods)]
-    
-    df_ts_non_instant = df_ts[df_ts['contact_channel_first'] != 'instant_book'].copy()
-
-    # Whether a booking occured or not
-    df_ts['booked'] = (~df_ts['ts_booking_at'].isna()).astype(int)
-
-    # Whether a guest is a previous booker or not
-    df_ts['past_booker'] = (df_ts['guest_user_stage_first'] == 'past_booker').astype(int)
-    df_ts['past_booker_booked'] = df_ts['past_booker'] * df_ts['booked']
-
-    # Whether a non-instant booking occured or not
-    df_ts_non_instant['booked'] = (~df_ts_non_instant['ts_booking_at'].isna()).astype(int)    
-
-    # Whether a non-instant guest is a previous booker or not
-    df_ts_non_instant['past_booker'] = (df_ts_non_instant['guest_user_stage_first'] == 'past_booker').astype(int)
-    df_ts_non_instant['past_booker_booked'] = df_ts_non_instant['past_booker'] * df_ts_non_instant['booked']
-
-    # Time for the host to respond to non-instant inquiry
-    df_ts_non_instant['response_time'] = df_ts_non_instant['ts_reply_at_first'] - df_ts_non_instant['ts_interaction_first']
-    
-    df_ts.set_index('ts_interaction_first', inplace = True)
-    df_ts_non_instant.set_index('ts_interaction_first', inplace = True)
-    
-    interval_data = df_ts.resample(interval).agg({
-        'id_guest_anon': 'count', # total inquiries
-        'booked': 'sum', # total booked
-        'past_booker': 'sum', # total previous bookers
-        'past_booker_booked': 'sum' # total bookings by previous bookers
-        })
-    
-    interval_data_non_instant = df_ts_non_instant.resample(interval).agg({
-        'id_guest_anon': 'count', # total non-instant inquiries
-        'ts_reply_at_first': 'count',
-        'booked': 'sum', # total non-instant booked
-        'past_booker': 'sum', # total non-instant previous nbookers
-        'past_booker_booked': 'sum', # total non-instant bookings by previous bookers
-        'm_first_message_length_in_characters': 'sum', # total guest message length
-        'm_interactions': 'sum', # total guest-host interaction
-        'response_time': 'mean' # average response time
-        })
-    
-    interval_data_final = pd.DataFrame()
-    interval_data_final.index = interval_data.index
-
-    ## Metrics
-    # Conversion Rate 
-    interval_data_final['cr'] = interval_data['booked'] / interval_data['id_guest_anon']
-
-    # Average Response Time
-    interval_data_final['rt'] = interval_data_non_instant['response_time'].dt.total_seconds()
-    interval_data_final['rt'] /= 3600 if interval == 'D' else 60
-
-    # Average Response Rate
-    interval_data_final['rr'] = interval_data_non_instant['ts_reply_at_first'] / interval_data_non_instant['id_guest_anon']
-
-    # Repeat Gueast Rate
-    interval_data_final['rgr'] = interval_data_non_instant['past_booker'] / interval_data_non_instant['id_guest_anon']
-
-    # Repeat booking Rate
-    interval_data_final['rbr'] = interval_data_non_instant['past_booker_booked'] / interval_data_non_instant['booked']
-
-    # Average length of guests' first message
-    interval_data_final['msg'] = interval_data_non_instant['m_first_message_length_in_characters'] / interval_data_non_instant['id_guest_anon']
-
-    # Average length of guest-host interaction
-    interval_data_final['ghi'] = interval_data_non_instant['m_interactions'] / interval_data_non_instant['id_guest_anon']
-
-    interval_data_final['day'] = interval_data_final.index.strftime('%A')
-
-    day_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    interval_data_final['day'] = pd.Categorical(interval_data_final['day'], categories = day_order, ordered = True)
-
-    if fix_outliers:
-        numeric_columns = interval_data_final.select_dtypes(include=['number']).columns
-        for col in numeric_columns:
-            median_i = interval_data_final[col].median()
-            iqr = interval_data_final[col].quantile(0.75) - interval_data_final[col].quantile(0.25)
-            lower_threshold = interval_data_final[col].quantile(0.25) - 1.5 * iqr
-            upper_threshold = interval_data_final[col].quantile(0.75) + 1.5 * iqr
-            outlier_condition = ~interval_data_final[col].between(lower_threshold, upper_threshold)
-            interval_data_final.loc[outlier_condition, col] = median_i
-
-    return interval_data_final
+    return master_df
