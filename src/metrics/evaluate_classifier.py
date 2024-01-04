decc233b14fa0f1d6d36f0ae7cdaffbe3f5690ab
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-04 13:52:33 -0500

    Update and rename evaluate_classifier.py to evaluate_classifier.py

--- src/metrics/evaluate_classifier.py
+++ src/metrics/evaluate_classifier.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+
 def evaluate_classifier(y_true, y_pred, scores = None):
   
   if scores is None:
