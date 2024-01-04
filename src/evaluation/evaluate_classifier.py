3ec39edaa600850927de286e151024297b3b442f
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-04 13:04:16 -0500

    Create evaluate_classifier.py

---
+++ src/evaluation/evaluate_classifier.py
@@ -0,0 +1,14 @@
+def evaluate_classifier(y_true, y_pred, scores = None):
+  
+  if scores is None:
+    scores = {'accuracy': accuracy_score,
+              'precision': precision_score,
+              'recall': recall_score,
+              'f1': f1_score,
+              'roc_auc': roc_auc_score}
+
+  result = {}
+  for name, func in scores.items():
+    result[name] = func(y_true, y_pred)
+
+  return result
