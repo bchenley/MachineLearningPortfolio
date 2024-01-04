895ea3a7ca1530110be32f794b3316a43fd65c69
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-04 12:53:53 -0500

    Create describe.py

---
+++ src/describe.py
@@ -0,0 +1,9 @@
+def describe(df):
+  df_ = pd.concat([df.dtypes, df.nunique()], axis = 1)
+  df_.columns = ['Dtype', 'Cardinality']
+  
+  df_['Unknown'] = 0
+  for col in df_.index:
+    df_.loc[df_.index == col, 'Unknown'] = (df[col] == 'unknown').sum()
+
+  return df_
