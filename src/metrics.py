8a8e4dee62c4722eba2aaf9054c820a245c25601
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-04 21:11:36 -0500

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, roc_curve
 
 def evaluate_classifier(y_true, y_pred, scores = None):
   
@@ -14,3 +14,25 @@ def evaluate_classifier(y_true, y_pred, scores = None):
     result[name] = func(y_true, y_pred)
 
   return result
+
+def get_classification_results(model, X, y):
+
+  # Labels
+  y_pred = model.predict(X)
+  # Probabilities
+  y_proba = model.predict_proba(X)[:,1]
+  ## metrics
+  metrics = evaluate_classifier(y, y_pred)
+  ## ROC curve
+  fpr, tpr, _ = roc_curve(y, y_proba, drop_intermediate = False)
+
+  results = {'pred': y_pred,
+             'proba': y_proba,
+             'accuracy': metrics['accuracy'],
+             'precision': metrics['precision'],
+             'f1': metrics['f1'],
+             'roc_auc': metrics['roc_auc'],
+             'fpr': fpr,
+             'tpr': tpr}
+
+  return results
