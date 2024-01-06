efe8d39a490d33d8993b875ed46b97813cd26f54
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-06 17:29:12 -0500

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,6 @@
-from sklearn.preprocessing import FunctionTransformer, StandardScaler, MinMaxScaler
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import FunctionTransformer, StandardScaler, MinMaxScaler
 
 def log_transform(X, y = None, small_constant = 1e-10):
   X_t = np.log(X + np.max([small_constant, X.min()]))    
@@ -8,3 +9,16 @@ def log_transform(X, y = None, small_constant = 1e-10):
 def sqrt_transform(X, y = None, small_constant = 1e-10):
   X_t = np.sqrt(X + np.max([small_constant, X.min()]))    
   return X_t
+
+def factorize(df, return_uniques = False):
+  df_factorized = df.copy()
+
+  if return_uniques:
+    categorical_uniques = {}
+  for col in df_factorized.select_dtypes(include = ['object']).columns:
+    codes, unqiues = pd.factorize(df_factorized[col])
+    df_factorized[col] = codes
+    if return_uniques:
+      categorical_uniques[col] = unqiues
+  
+  return df_factorized, categorical_uniques if return_uniques else df_factorized
