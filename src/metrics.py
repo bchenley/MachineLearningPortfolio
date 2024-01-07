98f4c0973744935f11d718ba584c5a13d0485830
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-06 20:00:29 -0500

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -36,3 +36,19 @@ def get_classification_results(model, X, y):
              'tpr': tpr}
 
   return results
+
+def plot_roc(fpr, tpr,
+             ax = None,
+             lw = 1,
+             include_diag = True):
+
+  if ax is None:
+    fig, ax = plt.subplots(figsize = (5, 5))
+
+  if include_diag:
+    ax.plot([0, 1], [0, 1], '--k', lw = 1., alpha = 0.5)
+
+  ax.plot(fpr, tpr, lw = lw)
+  ax.grid(True)
+  ax.set_xlim([-0.01, 1.01])
+  ax.set_ylim([-0.01, 1.01])
