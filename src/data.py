27f1fa8c3a2a1e2b4e3b4da16457b774e125714d
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-08 15:45:15 -0500

    Update data.py

--- src/data.py
+++ src/data.py
@@ -7,8 +7,8 @@ class BasicDataset(Dataset):
                  X, y, 
                  X_dtype = torch.float32, y_dtype = torch.long):
 
-        X = torch.from_numpy(X.replace({False: 0, True: 1}).values if isinstance(X, pd.DataFrame) else X).to(X_dtype).squeeze()
-        y = torch.from_numpy(y.replace({False: 0, True: 1}).values if isinstance(y, pd.DataFrame) else y).to(y_dtype).squeeze()
+        X = torch.from_numpy(X.replace({False: 0, True: 1}).values if isinstance(X, pd.DataFrame) else np.array(X)).to(X_dtype).squeeze()
+        y = torch.from_numpy(y.replace({False: 0, True: 1}).values if isinstance(y, pd.DataFrame) else np.array(y)).to(y_dtype).squeeze()
         
         self.X, self.X_dtype = X, X_dtype
         self.y, self.y_dtype = y, y_dtype
