e590f3e5b2ff7a18da46c19e786d0a62bd7035a6
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-16 13:52:07 -0500

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -22,3 +22,28 @@ def factorize(df, return_uniques = False):
       categorical_uniques[col] = unqiues.tolist()
   
   return (df_factorized, categorical_uniques) if return_uniques else df_factorized
+
+def create_bins(data, method = 'sturges', num_bins = None):
+
+  N = len(data)
+  
+  min, max = np.min(data), np.max(data)
+  iqr = stats.iqr(data) 
+
+  if num_bins is None:
+    if method == 'sturges':
+      num_bins = int(1 + np.sqrt(N))
+    elif method == 'freedman_diaconis':
+      num_bins = int((max - min) / (2 * iqr / N**(1/3)))  
+    elif method == 'sqrt':
+      num_bins = int(np.qrt(N))
+
+  # Calculate the bin edges
+  bins = np.linspace(min, max, num_bins + 1)
+
+  # Create the histogram
+  data_binned, _ = np.histogram(data, bins = bins)
+  
+  bin_mapping = np.digitize(data, bins)
+
+  return bin_mapping, bins
