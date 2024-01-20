e042905262d3a3bfbb10155b7cee0a784f3bd4b2
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-20 15:23:59 -0500

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -73,59 +73,35 @@ def plot_roc(fpr, tpr,
   ax.set_xlim([-0.01, 1.01])
   ax.set_ylim([-0.01, 1.01])
 
-class Criterion():
+class RMSELoss(torch.nn.modules.loss._Loss):
+  def __init__(self, reduction = 'mean'):
+    super(RMSELoss, self).__init__()
 
-  def __init__(self, name='mse', dims=None):
+    self.reduction = reduction
+    self.name = 'RMSELoss'
 
-    self.name = name
-    self.dims = dims
+  def forward(self, input, target):
 
-  def __call__(self, y_pred, y_true, num_params = None):
+    loss = torch.nn.MSLELoss(reduction = self.reduction)(input, target).sqrt()
+
+    return loss
+
+class RMSLELoss(torch.nn.modules.loss._Loss):
+  def __init__(self, reduction = 'mean'):
+    super(RMSLELoss, self).__init__()
+
+    self.reduction = reduction
+    self.name = 'RMSLELoss'
+
+  def forward(self, input, target):
+
+    if (input < 0).any() & ~(input < 0).any():
+      raise ValueError("input has negative values")
+    elif ~(input < 0).any() & (input < 0).any():
+      raise ValueError("target has negative values")
+    elif (input < 0).any() & (input < 0).any():
+      raise ValueError("both the input and target have negative values")
     
-    if self.name == 'mae':
-        # Mean Absolute Error (L1 loss)
-        if self.dims is not None: criterion = (y_true - y_pred).abs().nanmean(dim = self.dims)
-        else: criterion = (y_true - y_pred).abs()
-    elif self.name == 'mse':
-        # Mean Squared Error
-        if self.dims is not None: criterion = (y_true - y_pred).pow(2).nanmean(dim = self.dims)
-        else: criterion = (y_true - y_pred).pow(2)
-    elif self.name == 'mase':
-        # Mean Absolute Scaled Error
-        if self.dims is not None: criterion = (y_true - y_pred).abs().nanmean(dim=self.dims) / (y_true.diff(n=1, dim=self.dims).abs().nanmean(dim=self.dims))
-        else: criterion = (y_true - y_pred).abs() / y_true.diff(n=1, dim=self.dims).abs()
-    elif self.name == 'rmse':
-        # Root Mean Squared Error
-        if self.dims is not None: criterion = (y_true - y_pred).pow(2).nanmean(dim = self.dims).sqrt()
-        else: criterion = (y_true - y_pred).pow(2).sqrt()
-    elif self.name == 'nmse':
-      # Normalized Mean Squared Error
-      if self.dims is not None: criterion = (y_true - y_pred).pow(2).nansum(dim = self.dims) / y_true.pow(2).nansum(dim=self.dims)
-      else: criterion = (y_true - y_pred).abs() / y_true.pow(2)
-    elif self.name == 'rmsle':
-      # Root mean squared logarithmic error
-      if self.dims is not None: criterion = ((y_true+1).log() - (y_pred+1).log()).pow(2).nanmean(dim = self.dims).sqrt()                                            
-      else: criterion = ((y_true+1).log() - (y_pred+1).log()).pow(2).sqrt()
-    elif self.name == 'mape':
-        # Mean Absolute Percentage Error
-        if self.dims is not None: criterion = (((y_true - y_pred) / y_true).abs() * 100).nanmean(dim = self.dims)
-        else: criterion = (((y_true - y_pred) / y_true).abs() * 100)    
-    elif self.name == 'fb':
-        # Fractional Bias
-        if self.dims is not None: criterion = (y_pred.nansum(dim=self.dims) - y_true.nansum(dim=self.dims)) / y_true.nansum(dim=self.dims) * 100
-        else: criterion = 2*(y_pred - y_true)/(y_pred + y_true) # (y_pred - y_true) / y_true * 100
-    elif self.name == 'bic':
-      N = torch.tensor(y_true.shape[0] if y_true.ndim == 2 else y_true.shape[1]).to(y_true)
-      
-      error_var = torch.var(y_true - y_pred)      
-      criterion = N*torch.log(error_var) + num_params*torch.log(N)
-    elif self.name == 'r2':
-        if self.dims is not None: 
-          if self.dims == (1):
-            y_true_mean = y_true.mean(dim = self.dims).unsqueeze(1).repeat(1, y_true.shape[1], 1)
-          else:
-            y_true_mean = y_true.mean(dim = self.dims)
-          criterion = 1 - (y_true - y_pred).pow(2).sum(dim = self.dims)/(y_true - y_true_mean).pow(2).sum(dim = self.dims)
-        else: criterion = 1 - (y_true - y_pred).pow(2)/(y_true - y_true.mean(dim = self.dims)).pow(2)
-        
-    return criterion
+    loss = torch.nn.MSELoss(reduction = self.reduction)(torch.log1p(input), torch.log1p(target)).sqrt()
+
+    return loss  
