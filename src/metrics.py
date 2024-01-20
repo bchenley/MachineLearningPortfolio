9ba5c8284f7f970816b6734af4d14162a210b8db
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-20 14:02:55 -0500

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -72,3 +72,60 @@ def plot_roc(fpr, tpr,
   ax.grid(True)
   ax.set_xlim([-0.01, 1.01])
   ax.set_ylim([-0.01, 1.01])
+
+class Criterion():
+
+  def __init__(self, name='mse', dims=None):
+
+    self.name = name
+    self.dims = dims
+
+  def __call__(self, y_pred, y_true, num_params = None):
+    
+    if self.name == 'mae':
+        # Mean Absolute Error (L1 loss)
+        if self.dims is not None: criterion = (y_true - y_pred).abs().nanmean(dim = self.dims)
+        else: criterion = (y_true - y_pred).abs()
+    elif self.name == 'mse':
+        # Mean Squared Error
+        if self.dims is not None: criterion = (y_true - y_pred).pow(2).nanmean(dim = self.dims)
+        else: criterion = (y_true - y_pred).pow(2)
+    elif self.name == 'mase':
+        # Mean Absolute Scaled Error
+        if self.dims is not None: criterion = (y_true - y_pred).abs().nanmean(dim=self.dims) / (y_true.diff(n=1, dim=self.dims).abs().nanmean(dim=self.dims))
+        else: criterion = (y_true - y_pred).abs() / y_true.diff(n=1, dim=self.dims).abs()
+    elif self.name == 'rmse':
+        # Root Mean Squared Error
+        if self.dims is not None: criterion = (y_true - y_pred).pow(2).nanmean(dim = self.dims).sqrt()
+        else: criterion = (y_true - y_pred).pow(2).sqrt()
+    elif self.name == 'nmse':
+      # Normalized Mean Squared Error
+      if self.dims is not None: criterion = (y_true - y_pred).pow(2).nansum(dim = self.dims) / y_true.pow(2).nansum(dim=self.dims)
+      else: criterion = (y_true - y_pred).abs() / y_true.pow(2)
+    elif self.name == 'msle':
+      # Root mean squared logarithmic error
+      if self.dims is not None: criterion = ((y_true+1).log() - (y_pred+1).log()).pow(2).nanmean(dim = self.dims).sqrt()                                            
+      else: criterion = ((y_true+1).log() - (y_pred+1).log()).pow(2).sqrt()
+    elif self.name == 'mape':
+        # Mean Absolute Percentage Error
+        if self.dims is not None: criterion = (((y_true - y_pred) / y_true).abs() * 100).nanmean(dim = self.dims)
+        else: criterion = (((y_true - y_pred) / y_true).abs() * 100)    
+    elif self.name == 'fb':
+        # Fractional Bias
+        if self.dims is not None: criterion = (y_pred.nansum(dim=self.dims) - y_true.nansum(dim=self.dims)) / y_true.nansum(dim=self.dims) * 100
+        else: criterion = 2*(y_pred - y_true)/(y_pred + y_true) # (y_pred - y_true) / y_true * 100
+    elif self.name == 'bic':
+      N = torch.tensor(y_true.shape[0] if y_true.ndim == 2 else y_true.shape[1]).to(y_true)
+      
+      error_var = torch.var(y_true - y_pred)      
+      criterion = N*torch.log(error_var) + num_params*torch.log(N)
+    elif self.name == 'r2':
+        if self.dims is not None: 
+          if self.dims == (1):
+            y_true_mean = y_true.mean(dim = self.dims).unsqueeze(1).repeat(1, y_true.shape[1], 1)
+          else:
+            y_true_mean = y_true.mean(dim = self.dims)
+          criterion = 1 - (y_true - y_pred).pow(2).sum(dim = self.dims)/(y_true - y_true_mean).pow(2).sum(dim = self.dims)
+        else: criterion = 1 - (y_true - y_pred).pow(2)/(y_true - y_true.mean(dim = self.dims)).pow(2)
+        
+    return criterion
