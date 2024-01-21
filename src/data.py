0589370df25d93a3c3f0c045e6aa4807831deb25
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-21 15:27:32 -0500

    Update data.py

--- src/data.py
+++ src/data.py
@@ -4,16 +4,35 @@ import pandas as pd
 class BasicDataset(torch.utils.data.Dataset):
     def __init__(self, 
                  X, y, 
+                 device = 'cpu',
                  X_dtype = torch.float32, y_dtype = torch.long):
 
-        X = torch.from_numpy(X.replace({False: 0, True: 1}).values if isinstance(X, pd.DataFrame) else np.array(X)).to(X_dtype).squeeze()
-        y = torch.from_numpy(y.replace({False: 0, True: 1}).values if isinstance(y, pd.DataFrame) else np.array(y)).to(y_dtype).squeeze()
+        if isinstance(X, pd.DataFrame):
+            X = X.copy().values
+        elif isinstance(X, pd.Series):
+            X = X.copy().values.reshape(-1, 1)
+        elif isinstance(X, np.ndarray):
+            X = X.copy()
+        elif isinstance(X, torch.Tensor):
+            X = X.clone().detach()
+
+        if isinstance(y, pd.DataFrame):
+            y = y.copy().values
+        elif isinstance(y, pd.Series):
+            y = y.copy().values.reshape(-1, 1)
+        elif isinstance(y, np.ndarray):
+            y = y.copy()
+        elif isinstance(y, torch.Tensor):
+            y = y.clone().detach()
+
+        X = torch.tensor(X).to(device = device, dtype = X_dtype)
+        y = torch.tensor(y).to(device = device, dtype = X_dtype)
         
         self.X, self.X_dtype = X, X_dtype
         self.y, self.y_dtype = y, y_dtype
 
     def __len__(self):
-        return len(self.X)
+      return len(self.X)
     
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
