da215661c97098ac1009a5e2030f0151c7333a58
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-30 21:28:50 -0500

    Create ensemble.py

---
+++ src/ensemble.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import kmeans_plusplus
+
+class CustomKMeans():
+    def __init__(self, 
+                 n_clusters = 2,
+                 init = 'k-means++',
+                 max_iter = 100,
+                 metric = 'euclidean',
+                 random_state = None):
+        
+        self.n_clusters = n_clusters
+        self.init = init
+        self.max_iter = max_iter
+        self.metric = metric
+        self.random_state = random_state
+        
+        if self.metric == 'euclidean':
+            self.metric_fn = euclidean_distance
+        elif self.metric == 'manhattan':
+            self.metric_fn = manhattan_distance
+        elif self.metric == 'cosine_similarity':
+            self.metric_fn = cosine_similarity
+        
+        self.cluster_centers_ = None
+        self.labels_ = None
+        
+    def assign_clusters(self, data):
+        
+        distances = []
+        for n in range(self.n_clusters):
+            distances.append(self.metric_fn(data, self.cluster_centers_[n]))
+        
+        distances = np.stack(distances)
+        
+        labels_ = distances.argmin(axis = 1)
+        
+        return labels_
+    
+    def update_cluster_centers(self, data):
+        
+        cluster_centers = []
+        for n, label in enumerate(range(self.n_clusters)):
+            if np.any(self.labels_ == label):
+                cluster_centers.append(data[self.labels_ == label, :].mean(axis = 0))
+            else:
+                cluster_centers.append(data[:, self.metric_fn(data, self.cluster_centers_[n]).argmax()])
+        
+        cluster_centers = np.stack(cluster_centers, axis = 0)
+        
+        return cluster_centers
+        
+    def fit(self, data):
+        
+        self.n_samples = data.shape[0]
+        
+        np.random.seed(self.random_state)
+        
+        if self.labels_ is None:
+            self.labels_ = np.random.randint(0, self.n_clusters, (self.n_samples,))
+        
+        if self.init == 'k-means++':
+            self.cluster_centers_, _ = kmeans_plusplus(data[X_name_num].values, n_clusters = self.n_clusters)
+        elif self.init == 'random':
+            self.cluster_centers_ = data[np.random.randint(0, data.shape[0], (self.n_clusters, data.shape[1]))]
+        
+        new_labels_ = self.assign_clusters(data, self.cluster_centers_)
+        
+        itr = 0
+        while (any(self.labels_ != new_labels_) and (itr <= self.max_iter)):
+            self.labels_ = new_labels_
+            self.cluster_centers_ = self.update_cluster_centers(data)
+            new_labels_ = self.assign_clusters(data)
+            itr += 1
+    
+    def predict(self, data):
+        return self.assign_clusters(data)
+            
