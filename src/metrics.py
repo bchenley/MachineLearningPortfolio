69ade4c881e0123bddbec8cdf0f52875b631fb32
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-30 21:34:13 -0500

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as 
+import numpy as np
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, \
                             f1_score, roc_auc_score, roc_curve, \
