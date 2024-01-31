cd7686d59ac01ac3a76e9225ede1d207cc565a3a
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-01-30 22:06:48 -0500

    Update ensemble.py

--- src/ensemble.py
+++ src/ensemble.py
@@ -19,12 +19,14 @@ class CustomKMeans():
         self.metric = metric
         self.random_state = random_state
         
+        self.greater_is_better = False
         if self.metric == 'euclidean':
             self.metric_fn = euclidean_distance
         elif self.metric == 'manhattan':
             self.metric_fn = manhattan_distance
-        elif self.metric == 'cosine_similarity':
+        elif self.metric == 'cosine_similarity':            
             self.metric_fn = cosine_similarity
+            self.greater_is_better = True
         
         self.cluster_centers_ = None
         self.labels_ = None
@@ -32,10 +34,10 @@ class CustomKMeans():
     def assign_clusters(self, data):
         
         distances = []
-        for n in range(self.n_clusters):
-            distances.append(self.metric_fn(data, self.cluster_centers_[n]))
+        for n in range(self.n_clusters):            
+            distances.append((-1)**(1 + ~self.greater_is_better) * self.metric_fn(data, self.cluster_centers_[n]))
         
-        distances = np.stack(distances)
+        distances = np.stack(distances, axis = 1)
         
         labels_ = distances.argmin(axis = 1)
         
@@ -64,12 +66,13 @@ class CustomKMeans():
             self.labels_ = np.random.randint(0, self.n_clusters, (self.n_samples,))
         
         if self.init == 'k-means++':
-            self.cluster_centers_, _ = kmeans_plusplus(data[X_name_num].values, n_clusters = self.n_clusters)
+            self.cluster_centers_, _ = kmeans_plusplus(data, n_clusters = self.n_clusters)
         elif self.init == 'random':
             self.cluster_centers_ = data[np.random.randint(0, data.shape[0], (self.n_clusters, data.shape[1]))]
         
-        new_labels_ = self.assign_clusters(data, self.cluster_centers_)
-        
+        new_labels_ = self.assign_clusters(data)
+
+
         itr = 0
         while (any(self.labels_ != new_labels_) and (itr <= self.max_iter)):
             self.labels_ = new_labels_
@@ -79,4 +82,3 @@ class CustomKMeans():
     
     def predict(self, data):
         return self.assign_clusters(data)
-            
