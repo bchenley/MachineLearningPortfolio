624460218e2ec0215d456565764a7619d415b8fe
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-02-01 00:27:37 -0500

    Update cluster.py

--- src/cluster.py
+++ src/cluster.py
@@ -10,22 +10,23 @@ class CustomKMeans():
                  n_clusters = 2,
                  init = 'k-means++',
                  max_iter = 100,
-                 metric = 'euclidean',
+                 distance = 'euclidean',
                  random_state = None):
 
         self.n_clusters = n_clusters
         self.init = init
         self.max_iter = max_iter
-        self.metric = metric
+        self.distance = distance
         self.random_state = random_state
 
         self.greater_is_better = False
-        if self.metric == 'euclidean':
-            self.metric_fn = euclidean_distance
-        elif self.metric == 'manhattan':
-            self.metric_fn = manhattan_distance
-        elif self.metric == 'cosine_similarity':
-            self.metric_fn = cosine_similarity
+        self.distance_fn = None
+        if self.distance == 'euclidean':
+            self.distance_fn = euclidean_distance
+        elif self.distance == 'manhattan':
+            self.distance_fn = manhattan_distance
+        elif self.distance == 'cosine_similarity':
+            self.distance_fn = cosine_similarity
             self.greater_is_better = True
 
         self.cluster_centers_ = None
@@ -35,7 +36,7 @@ class CustomKMeans():
 
         distances = []
         for n in range(self.n_clusters):
-            distances.append((-1)**(1 + ~self.greater_is_better) * self.metric_fn(data, self.cluster_centers_[n]))
+            distances.append((-1)**(1 + ~self.greater_is_better) * self.distance_fn(data, self.cluster_centers_[n]))
 
         distances = np.stack(distances, axis = 1)
 
@@ -50,7 +51,7 @@ class CustomKMeans():
             if np.any(self.labels_ == self.labels_[n]):
                 cluster_centers.append(data[self.labels_ == self.labels_[n], :].mean(axis = 0))
             else:
-                cluster_centers.append(data[:, ((-1)**(1 + ~self.greater_is_better)*self.metric_fn(data, self.cluster_centers_[n])).argmax()])
+                cluster_centers.append(data[:, ((-1)**(1 + ~self.greater_is_better)*self.distance_fn(data, self.cluster_centers_[n])).argmax()])
 
         cluster_centers = np.stack(cluster_centers, axis = 0)
 
@@ -78,7 +79,7 @@ class CustomKMeans():
             self.cluster_centers_ = self.update_cluster_centers(data)
             new_labels_ = self.assign_clusters(data)
             itr += 1
-
+    
     def predict(self, data):
       return self.assign_clusters(data)
 
@@ -87,3 +88,56 @@ class CustomKMeans():
       self.fit(data)
 
       return self.predict(data)
+    
+    def get_params(self, deep = False):
+
+      return {'n_clusters': self.n_clusters,
+              'init': self.init,
+              'max_iter': self.max_iter,
+              'distance': self.distance,
+              'random_state': self.random_state}
+
+    def set_params(self, params):
+      
+      for param, value in params.items():
+        setattr(self, param, value)
+      
+      return self
+
+    def elbow_test(self,
+                   train_data, val_data = None,
+                   Ks = [k for k in range(2, 6)], 
+                   distances = ['euclidean'],
+                   scores = ['inertia']):
+      
+      val_data = val_data or train_data
+
+      init_params = self.get_params()
+
+      results = {'n_clusters': [], 'distance': []}
+
+      for score in scores: 
+        results[score] = []
+
+      for distance in distances:
+        for n_clusters in Ks:
+
+          self.set_params({'n_clusters': n_clusters,
+                           'distance': distance})
+          
+          self.fit(train_data)
+
+          labels = self.predict(val_data)
+
+          scores_ = calculate_cluster_scores(data = val_data, labels = labels, distance = distance)
+
+          results['n_clusters'].append(n_clusters)
+          results['distance'].append(distance)
+          
+          for score in scores:            
+            results[score].append(scores_[score])
+
+      # Set params back to init values
+      self.set_params(init_params)
+
+      return results
