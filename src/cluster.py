d3c4fc25aac9fb85eb37cb430b990a8971afee74
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-02-01 12:48:26 -0500

    Update cluster.py

--- src/cluster.py
+++ src/cluster.py
@@ -3,7 +3,8 @@ import numpy as np
 
 from sklearn.cluster import kmeans_plusplus
 
-from metrics import euclidean_distance, manhattan_distance, cosine_similarity
+from metrics import euclidean_distance, within_cluster_sum_of_squares, \
+                    manhattan_distance, cosine_similarity
 
 class CustomKMeans():
     def __init__(self,
