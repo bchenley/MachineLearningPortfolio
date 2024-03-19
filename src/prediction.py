8a5722dbef0bf177d5bf450439024acae7fda5f4
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-03-18 22:15:09 -0500

    Create prediction.py

---
+++ src/prediction.py
@@ -0,0 +1,25 @@
+from . import transform_data, inverse_transform_data
+
+def predict(model, X, 
+            hiddens = False,
+            X_transformer = None,
+            y_transformer = None):
+
+  if X_transformer is not None:
+    X = transform_data(X, X_transformer)
+
+  with torch.no_grad():
+    if hiddens is False:
+      y_pred = model(X)
+    else:
+      if isinstance(hiddens, tuple):
+        hiddens = [h[:, :X.shape[0]].contiguous() for h in hiddens]
+      else:
+        hiddens = None
+      
+      y_pred, hiddens = model(X, hiddens = hiddens)
+
+    if y_transformer is not None:
+      y_pred = inverse_transform_data(y_pred, y_transformer)
+
+  return y_pred, hiddens
