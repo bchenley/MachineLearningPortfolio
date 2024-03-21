d75be8fdd03682ee2858d388ebab221d69b49878
Brandon Henley <47284460+bchenley@users.noreply.github.com>
2024-03-20 20:34:07 -0500

    Update neural_networks.py

--- src/neural_networks.py
+++ src/neural_networks.py
@@ -136,114 +136,138 @@ class CustomCNN1D(torch.nn.Module):
 # 2D CNN
 class CustomCNN2D(torch.nn.Module):
     
-    def __init__(self, 
-                 in_channels, out_channels,
-                 causal_pad = False,
-                 kernel_size = [(1, 1)], kernel_stride = [(1, 1)], padding = [(0, 0)], 
-                 dilation = [(1, 1)], groups = [1], bias = [True], 
-                 pool_type = [None], pool_size = [(2, 2)], pool_stride = [(1, 1)],
-                 activation = ['identity'],
-                 degree = [2], coef_init = [None], coef_train = [True], coef_reg = [[0.001, 1]] , zero_order = [False],
-                 batch_norm = False, batch_norm_learn = False,
-                 dropout_p = [0.],
-                 device = None, dtype = None):
+  def __init__(self, 
+                in_channels, out_channels,
+                kernel_size = [(1, 1)], kernel_stride = [(1, 1)], padding = [(0, 0)], 
+                dilation = [(1, 1)], groups = [1], bias = [True], 
+                pool_type = [None], pool_size = [(2, 2)], pool_stride = [(1, 1)],
+                activation = ['identity'],
+                degree = [2], coef_init = [None], coef_train = [True], coef_reg = [[0.001, 1]] , zero_order = [False],
+                batch_norm = False, batch_norm_learn = False,
+                dropout_p = [0.],
+                device = None, dtype = None):
 
-        super(CustomCNN2D, self).__init__()
-        
-        # Store the arguments as attributes
-        locals_ = locals().copy()
-        for arg in locals_:
-            if arg != 'self':
-                setattr(self, arg, locals_[arg])
+      super(CustomCNN2D, self).__init__()
+      
+      # Store the arguments as attributes
+      locals_ = locals().copy()
+      for arg in locals_:
+          if arg != 'self':
+              setattr(self, arg, locals_[arg])
 
-        self.num_layers = len(self.out_channels)
+      self.num_layers = len(self.out_channels)
 
-        # Replicate single-value parameters to match the number of layers
-        if len(self.kernel_size) == 1: self.kernel_size = self.kernel_size * self.num_layers
-        if len(self.kernel_stride) == 1: self.kernel_stride = self.kernel_stride * self.num_layers
-        if len(self.padding) == 1: self.padding = self.padding * self.num_layers
-        if len(self.dilation) == 1: self.dilation = self.dilation * self.num_layers
-        if len(self.groups) == 1: self.groups = self.groups * self.num_layers
-        if len(self.bias) == 1: self.bias = self.bias * self.num_layers
-        if len(self.activation) == 1: self.activation = self.activation * self.num_layers
-        if len(self.degree) == 1: self.degree = self.degree * self.num_layers
-        if len(self.coef_init) == 1: self.coef_init = self.coef_init * self.num_layers
-        if len(self.coef_train) == 1: self.coef_train = self.coef_train * self.num_layers
-        if len(self.coef_reg) == 1: self.coef_reg = self.coef_reg * self.num_layers
-        if len(self.zero_order) == 1: self.zero_order = self.zero_order * self.num_layers
-        if len(self.pool_type) == 1: self.pool_type = self.pool_type * self.num_layers
-        if len(self.pool_size) == 1: self.pool_size = self.pool_size * self.num_layers
-        if len(self.pool_stride) == 1: self.pool_stride = self.pool_stride * self.num_layers
-        if len(self.dropout_p) == 1: self.dropout_p = self.dropout_p * self.num_layers
-            
-        # Create the CNN layers
-        self.cnn = torch.nn.ModuleList()  
-        for i in range(self.num_layers):
-            self.cnn.append(torch.nn.Sequential())
-            
-            # Determine the input channels for the current layer
-            in_channels_i = self.in_channels if i == 0 else self.out_channels[i - 1]
+      # Replicate single-value parameters to match the number of layers
+      if len(self.kernel_size) == 1: self.kernel_size = self.kernel_size * self.num_layers
+      if len(self.kernel_stride) == 1: self.kernel_stride = self.kernel_stride * self.num_layers
+      if len(self.padding) == 1: self.padding = self.padding * self.num_layers
+      if len(self.dilation) == 1: self.dilation = self.dilation * self.num_layers
+      if len(self.groups) == 1: self.groups = self.groups * self.num_layers
+      if len(self.bias) == 1: self.bias = self.bias * self.num_layers
+      if len(self.activation) == 1: self.activation = self.activation * self.num_layers
+      if len(self.degree) == 1: self.degree = self.degree * self.num_layers
+      if len(self.coef_init) == 1: self.coef_init = self.coef_init * self.num_layers
+      if len(self.coef_train) == 1: self.coef_train = self.coef_train * self.num_layers
+      if len(self.coef_reg) == 1: self.coef_reg = self.coef_reg * self.num_layers
+      if len(self.zero_order) == 1: self.zero_order = self.zero_order * self.num_layers
+      if len(self.pool_type) == 1: self.pool_type = self.pool_type * self.num_layers
+      if len(self.pool_size) == 1: self.pool_size = self.pool_size * self.num_layers
+      if len(self.pool_stride) == 1: self.pool_stride = self.pool_stride * self.num_layers
+      if len(self.dropout_p) == 1: self.dropout_p = self.dropout_p * self.num_layers
+          
+      # Create the CNN layers
+      self.cnn = torch.nn.ModuleList()  
+      for i in range(self.num_layers):
+          self.cnn.append(torch.nn.Sequential())
+          
+          # Determine the input channels for the current layer
+          in_channels_i = self.in_channels if i == 0 else self.out_channels[i - 1]
 
-            # 1) Add Conv2d layer to the current layer
-            self.cnn[-1].append(torch.nn.Conv2d(in_channels = in_channels_i,
-                                                out_channels = self.out_channels[i],
-                                                kernel_size = self.kernel_size[i],
-                                                stride = self.kernel_stride[i],
-                                                padding = self.padding[i],
-                                                dilation = self.dilation[i],
-                                                groups = self.groups[i],
-                                                bias = self.bias[i],
-                                                device = self.device, dtype = self.dtype))
-            
-            # 2) Add batch normalization layer if specified
-            if self.batch_norm:
-                batch_norm_i = torch.nn.BatchNorm2d(self.out_channels[i], 
-                                                    affine = self.batch_norm_learn,
-                                                    device = self.device, dtype = self.dtype)
-            else:
-                batch_norm_i = torch.nn.Identity()
-            
-            self.cnn[-1].append(batch_norm_i)
-            
-            # 3) Add activation
-            if isinstance(self.activation[i], torch.nn.Module):
-              pass
-            if (self.activation[i] == 'identity') or (self.activation[i] is None):
-              activation_fn = torch.nn.Identity()
-            elif self.activation[i] == 'relu':
-              activation_fn = torch.nn.ReLU()
-            elif self.activation[i] == 'sigmoid':
-              activation_fn = torch.nn.Sigmoid()
-            elif self.activation[i] == 'softmax':
-              activation_fn = torch.nn.Softmax(dim = -1)
-            elif self.activation[i] == 'tanh':
-              activation_fn = torch.nn.Tanh()  
-            elif self.activation[i] == 'polynomial':
-              activation_fn = Polynomial(in_features = self.out_channels[i],
-                                         degree = self.degree[i],
-                                         coef_init = self.coef_init[i],
-                                         coef_train = self.coef_train[i],
-                                         coef_reg = self.coef_reg[i],
-                                         zero_order = self.zero_order[i],
-                                         device = self.device,
-                                         dtype = self.dtype)
-            
-            self.cnn[-1].append(activation_fn)
-            
-            # 4) Add pooling layer if specified
-            if self.pool_type[i] is None:
-                pool_i = torch.nn.Identity()
-            if self.pool_type[i] == 'max':
-                pool_i = torch.nn.MaxPool2d(kernel_size = self.pool_size[i],
-                                            stride = self.pool_stride[i])
-            elif self.pool_type[i] == 'avg':
-                pool_i = torch.nn.AvgPool2d(kernel_size = self.pool_size[i],
-                                            stride = self.pool_stride[i])     
+          # 1) Add Conv2d layer to the current layer
+          self.cnn[-1].append(torch.nn.Conv2d(in_channels = in_channels_i,
+                                              out_channels = self.out_channels[i],
+                                              kernel_size = self.kernel_size[i],
+                                              stride = self.kernel_stride[i],
+                                              padding = self.padding[i],
+                                              dilation = self.dilation[i],
+                                              groups = self.groups[i],
+                                              bias = self.bias[i],
+                                              device = self.device, dtype = self.dtype))
+          
+          # 2) Add batch normalization layer if specified
+          if self.batch_norm:
+              batch_norm_i = torch.nn.BatchNorm2d(self.out_channels[i], 
+                                                  affine = self.batch_norm_learn,
+                                                  device = self.device, dtype = self.dtype)
+          else:
+              batch_norm_i = torch.nn.Identity()
+          
+          self.cnn[-1].append(batch_norm_i)
+          
+          # 3) Add activation
+          if isinstance(self.activation[i], torch.nn.Module):
+            pass
+          if (self.activation[i] == 'identity') or (self.activation[i] is None):
+            activation_fn = torch.nn.Identity()
+          elif self.activation[i] == 'relu':
+            activation_fn = torch.nn.ReLU()
+          elif self.activation[i] == 'sigmoid':
+            activation_fn = torch.nn.Sigmoid()
+          elif self.activation[i] == 'softmax':
+            activation_fn = torch.nn.Softmax(dim = -1)
+          elif self.activation[i] == 'tanh':
+            activation_fn = torch.nn.Tanh()  
+          elif self.activation[i] == 'polynomial':
+            activation_fn = Polynomial(in_features = self.out_channels[i],
+                                        degree = self.degree[i],
+                                        coef_init = self.coef_init[i],
+                                        coef_train = self.coef_train[i],
+                                        coef_reg = self.coef_reg[i],
+                                        zero_order = self.zero_order[i],
+                                        device = self.device,
+                                        dtype = self.dtype)
+          
+          self.cnn[-1].append(activation_fn)
+          
+          # 4) Add pooling layer if specified
+          if self.pool_type[i] is None:
+              pool_i = torch.nn.Identity()
+          if self.pool_type[i] == 'max':
+              pool_i = torch.nn.MaxPool2d(kernel_size = self.pool_size[i],
+                                          stride = self.pool_stride[i])
+          elif self.pool_type[i] == 'avg':
+              pool_i = torch.nn.AvgPool2d(kernel_size = self.pool_size[i],
+                                          stride = self.pool_stride[i])     
 
-            self.cnn[-1].append(pool_i)
+          self.cnn[-1].append(pool_i)
 
-            # 5) Dropout
-            self.cnn[-1].append(torch.nn.Dropout(self.dropout_p[i]))
+          # 5) Dropout
+          self.cnn[-1].append(torch.nn.Dropout(self.dropout_p[i]))
+
+  def forward(self, input):
+
+    output = input.clone()
+    for i in range(self.num_layers):   
+      # Apply the current CNN layer to the input tensor
+      output = self.cnn[i][0](output)
+      # Apply batch normalization
+      output = self.cnn[i][1](output)
+      # Apply activation
+      output = self.cnn[i][2](output)
+      # Apply pooling
+      output = self.cnn[i][3](output)
+      # Apply dropout
+      output = self.cnn[i][4](output)
+      
+    # Transpose back the output tensor to the original shape
+    output = output
+
+  def predict(self, input):
+
+    with torch.no_grad():
+      output = self.forward(input)
+      
+    return output
             
 ## Custom RNN network
 class CustomRNN(torch.nn.Module):
